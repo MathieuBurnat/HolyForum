44215830cf4d1ea4ccd87cf4d205d8ddeb41beab
Mathieu <mathieu.burnat@cpnv.ch>
2021-02-11 15:56:44 +0100

    Create layout with a basic footer and header

---
+++ resources/views/component/header.blade.php
@@ -0,0 +1,6 @@
+<head>
+    <meta charset="UTF-8">
+    <title>Holy Forum</title>
+    <h2> <- Header ->  </h2>
+</head>
+
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  @include("component/header")
+
+
+  <body>
+    <h1> Hello everyone ! </h1>
+    <!-- JavaScript -->
+
+  </body>
+
+    @include("component/footer")
+</html>
