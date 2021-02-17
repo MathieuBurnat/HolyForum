1a184c7053456a02172188e9a15100a67310deb1
Mathieu <mathieu.burnat@cpnv.ch>
2021-02-17 20:47:34 +0100

    Update footer.blade.php

--- resources/views/component/footer.blade.php
+++ resources/views/component/footer.blade.php
@@ -1,6 +1,5 @@
 <footer class="bg-light text-center text-lg-start fixed-bottom">
-    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
-      <p class="text-dark m-2"> V0.1 - Hamered by Mathieu Burnat </p>
-      
-    </div>
-  </footer>
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+    <p class="text-dark m-2"> V0.1 - Hammered by Mathieu Burnat </p>
+  </div>
+</footer>
