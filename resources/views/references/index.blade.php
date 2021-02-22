f745411b8dfc499f102f57b156e9d44fad68ade1
Mathieu <mathieu.burnat@cpnv.ch>
2021-02-23 00:15:11 +0100

    Upgrade the view's render

--- resources/views/references/index.blade.php
+++ resources/views/references/index.blade.php
@@ -3,30 +3,47 @@
 @section ('content')
     <h1> Références 0:') </h1>
 
-    <table class="w-100">
+    <table style="width: 50%">
         <thead>
-            <tr>
-                <th class="blue-gradient-rgba">Description</th>
+            <tr class="gradient">
+                <th>Description</th>
                 <th> </th>
             </tr>
         </thead>
         <tbody>
             @forelse ($references as $reference)
                 <tr>
-                    <td class="w-25">
+                    <td>
                         {{ $reference->description }}
                     </td>
-                    <td>
-                        <a href="$reference->url" target="_blank"><i class="fas fa-link"></i></a>
-                        <a href="?controller=reference&action=show&id=$reference->id"><i class="fas fa-eye"></i></a>
-                        <a href="?controller=reference&action=edit&id=$reference->id"><i class="fas fa-edit"></i></a>
+                    <td class="col-icons" style="float: right;">
+                        <a href="{{ $reference->url }}"><i class="fas fa-link"></i></a>
+                        <a href="{{ route('references.show', $reference->id) }}"><i class="fas fa-eye"></i></a>
+                        <a href="{{ route('references.show', $reference->id) }}"><i class="fas fa-edit"></i></a>
                     </th>
                 </tr>
             @empty
                 <div>Aucunes références ont été trouvées >:C </div>
             @endforelse
         </tbody>
-    </table>
 
-    <a href="{{ route('references.create') }}" class="btn btn-primary">Ajouter</a>
+    </table>
+    <a href="{{ route('references.create') }}" class="m-1 btn btn-secondary">Créer une nouvelle référence</a>
+
 @endsection
+
+<style>
+tr:hover > .col-icons {
+    opacity: 1;
+}
+
+.col-icons {
+    opacity: 0;
+}
+
+.gradient {
+    background: rgb(2,0,36);
+    background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 7%, rgba(0,212,255,1) 100%);
+    color: white;
+}
+</style>
