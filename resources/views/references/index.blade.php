bdfc9175340229fac87d163925682c44a0b991ba
Mathieu <mathieu.burnat@cpnv.ch>
2021-02-22 23:44:18 +0100

    Adding a better render

--- resources/views/references/index.blade.php
+++ resources/views/references/index.blade.php
@@ -1,47 +1,32 @@
 @extends ('layout')
 
 @section ('content')
-    <h1> Références 0:')  </h1> <span class="fas fa-eye"></span>
+    <h1> Références 0:') </h1>
 
+    <table class="w-100">
+        <thead>
+            <tr>
+                <th class="blue-gradient-rgba">Description</th>
+                <th> </th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($references as $reference)
+                <tr>
+                    <td class="w-25">
+                        {{ $reference->description }}
+                    </td>
+                    <td>
+                        <a href="$reference->url" target="_blank"><i class="fas fa-link"></i></a>
+                        <a href="?controller=reference&action=show&id=$reference->id"><i class="fas fa-eye"></i></a>
+                        <a href="?controller=reference&action=edit&id=$reference->id"><i class="fas fa-edit"></i></a>
+                    </th>
+                </tr>
+            @empty
+                <div>Aucunes références ont été trouvées >:C </div>
+            @endforelse
+        </tbody>
+    </table>
 
-    <div class="row">
-      
-        @forelse ($references as $reference)
-            <div class="col">Name : {{ $reference->description }} </div>
-            <div class="col">URL : {{ $reference->url }} </div>
-            <div class="w-100"></div>
-        @empty
-            <div>Aucunes références ont été trouvées >:C </div>
-        @endforelse
-    </div>
-
-    @forelse ($references as $reference)
-        <tr>
-            <th scope="col" class="col-icons col-fit-content">
-                <a href="$reference->url" target="_blank"><i class="fas fa-link"></i></a>
-                <a href="?controller=reference&action=show&id=$reference->id"><i class="fas fa-eye"></i></a>
-                <a href="?controller=reference&action=edit&id=$reference->id"><i class="fas fa-edit"></i></a>
-            </th>
-            <td>
-                {{ $reference->description }}
-            </td>
-        </tr>
-    @empty
-        <div>Aucunes références ont été trouvées >:C </div>
-    @endforelse
-
-
-        <!--div class="row" data-id="{{$reference->id}}">
-            <div class="col-2 text-right">
-                <div id="divIcons{{$reference->id}}" class="d-none">
-                    <a class="blacklink m-2" href="{{ $reference->url }}"><i class="fas fa-eye"></i></a>
-                    <a class="blacklink m-2" href="{{ route('references.show',$reference->id) }}"><i class="fas fa-search-plus"></i></a>
-                </div>
-            </div>
-            <div class="col-10 text-left">
-                {{ $reference->description }}
-            </div>
-        </div -->
-    
     <a href="{{ route('references.create') }}" class="btn btn-primary">Ajouter</a>
 @endsection
