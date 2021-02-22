1229f54f3033cab016e6bc5fae8cf60e922497af
Mathieu <mathieu.burnat@cpnv.ch>
2021-02-22 22:50:03 +0100

    Include font-awesome

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,6 +2,8 @@
 <html lang="en">
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <link type="text/css" rel="stylesheet" href="{{ mix('css/app.css') }}">
+
   @include("component/header")
 
   
