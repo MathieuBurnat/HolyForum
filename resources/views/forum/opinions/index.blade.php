910a627457802e4cb2d42db9c334dffcb8119cca
Mathieu <mathieu.burnat@cpnv.ch>
2021-03-03 19:40:26 +0100

    Upgrade the display of the comments

--- resources/views/forum/opinions/index.blade.php
+++ resources/views/forum/opinions/index.blade.php
@@ -5,14 +5,26 @@
     <h1> << [{{ $topic->user->pseudo }}] {{ $topic->description }} >> </h1>
 
     @forelse ($opinions as $opinion)
-        <h2> *{{ ($loop->index) + 1 }}* [{{ $opinion->user->pseudo }}] </h2>
-        <h3> {{ $opinion->description }} </h3>
+        <div class="card">
+            <h4 class="card-header"> #{{ ($loop->index) + 1 }} {{ $opinion->user->pseudo }} </h4>
+            <div class="card-body">
+                <h5 class="card-title">{{ $opinion->description }}</h5>
+                <a href="#" class="btn btn-primary">Répondre</a>
+        </div>
+
         @foreach ($opinion->comments as $comment)
-        <h5> {{ $comment->pseudo }}: {{ $comment->pivot->comment }}, {{ $comment->pivot->points }}pts</h5>
+            <div class="card" style="margin-left: 1em">
+                <h6 class="card-header"> {{ $comment->pseudo }} </h6>
+                <div class="card-body">
+                    <p class="card-text">{{ $comment->pivot->comment }} {{ $comment->pivot->points }}pts</p>
+                </div>
+            </div>
         @endforeach
     @empty
         <div>Aucunes opiniond n'ont été trouvées >:C </div>
     @endforelse
+
+
 
 @endsection
 
