3027ee9280a51f440fe9d09632419e5a56af68a2
Mathieu <mathieu.burnat@cpnv.ch>
2021-03-03 23:23:16 +0100

    Update dirties

--- resources/views/forum/opinions/index.blade.php
+++ resources/views/forum/opinions/index.blade.php
@@ -12,19 +12,19 @@
 
     @forelse ($opinions as $opinion)
         <div class="card">
-            <h5 class="card-header">  {{ $opinion->user->pseudo }} </h5>
+            <h5 class="card-header"> {{ $opinion->user->pseudo }} </h5>
             <div class="card-body">
-                <h6 class="card-title">{{ $opinion->description }}</h6>
-                <p>
-                    <button class="btn btn-outline-info" type="button" data-bs-toggle="collapse" data-bs-target="#collapseCard{{ ($loop->index) + 1 }}" aria-expanded="false" aria-controls="collapseExample">
-                        Répondre
-                    </button>
-                </p>
-                <div class="collapse" id="collapseCard{{ ($loop->index) + 1 }}">
-                    <div class="card card-body">
-                        @include('forum.opinions.newComment')
-                    </div>
+            <h6 class="card-title">{{ $opinion->description }}</h6>
+            <p>
+                <button class="btn btn-outline-info" type="button" data-bs-toggle="collapse" data-bs-target="#collapseCard{{ ($loop->index) + 1 }}" aria-expanded="false" aria-controls="collapseExample">
+                    Répondre
+                </button>
+            </p>
+            <div class="collapse" id="collapseCard{{ ($loop->index) + 1 }}">
+                <div class="card card-body">
+                    @include('forum.opinions.newComment')
                 </div>
+            </div>
         </div>
 
         @foreach ($opinion->comments as $comment)
