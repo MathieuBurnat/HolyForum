2d484da49165fbed1b6ac4bf00f8973a2f48eac3
Mathieu <70205357+MathieuBurnat@users.noreply.github.com>
2021-03-04 22:04:14 +0100

    Upgrade the error's view

--- resources/views/errors/not-admin.blade.php
+++ resources/views/errors/not-admin.blade.php
@@ -1,5 +1,10 @@
 @extends ('layout')
 
 @section ('content')
-    <h1> I'm sorry, you need to be an administrator to access this page. </h1>
+        <div class="text-center mt-5 ">
+            <h1 class="fst-italic"> Oops!</h1>
+            <h2 class="text-danger"> Erreur : Privilège Administrateur</h2>
+            Il semblerait que vous ne possédiez pas les droits suffisants pour accéder à cette page.
+        </div>
+
 @endsection
