17599165936bc57e5ef888b9c9bd807bbade2fef
Mathieu <70205357+MathieuBurnat@users.noreply.github.com>
2021-03-04 11:23:15 +0100

    Upgrade the "my account" onglet

--- resources/views/component/header.blade.php
+++ resources/views/component/header.blade.php
@@ -31,7 +31,7 @@
 
     </div>
 
-    <div class="collapse navbar-collapse navbar-right" id="navbarNavDropdown" style="position: absolute; right: 0px; padding:3em;">
+    <div class="collapse navbar-collapse navbar-right" id="navbarNavDropdown" style="position: absolute; right: 0px; padding:4em;">
       <!--a class="navbar-brand text-primary" href="{ route('login') }}">Login</a-->
       @if (auth::check())
         <ul class="navbar-nav">
@@ -40,15 +40,27 @@
               Mon compte
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <a class="dropdown-item text-danger" href="{{ route('logout') }}">Logout</a>
-              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item " href="#"> <h6>Votre Rôle</h6></a>
+                @switch(auth::User()->role->slug)
+                @case("PROF")
+                  <a class="dropdown-item text-primary" href="#"> Professeur </a>
+                  @break
+                @case("STUD")
+                  <a class="dropdown-item text-secondary" href="#"> Étudient </a>
+                  @break
+                @case("ADMI")
+                  <a class="dropdown-item text-warning" href="#"> Administrateur </a>
+                  @break
+                @endswitch
+              <!--a class="dropdown-item" href="#">Informations</a-->
               <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Something else here</a>
+              <a class="dropdown-item " href="#"> <h6>Options</h6></a>
+              <a class="dropdown-item text-danger" href="{{ route('logout') }}">Logout</a>
             </div>
           </li>
         </ul>
       @else
-        <a class="navbar-brand text-primary" href="{{ route('login') }}">Login</a>
+        <a class="navbar-brand" href="{{ route('login') }}">Login</a>
       @endif
       
     </div>
