7d402c051a46829216c5f7bf712a5ad4b18e9f79
Mathieu <70205357+MathieuBurnat@users.noreply.github.com>
2021-03-05 00:16:49 +0100

    Upgrade interface when errors, add theme tag

--- resources/views/forum/themes/index.blade.php
+++ resources/views/forum/themes/index.blade.php
@@ -1,31 +1,32 @@
 @extends ('layout')
 
 @section ('content')
-<h1> Liste des thèmes </h1>
+    <h1> Liste des thèmes </h1>
 
-<table class="table">
-    <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Nom</th>
-            <th scope="col"></th>
-        </tr>
-    </thead>
-    <tbody>
-        @forelse ($themes as $theme)
-        <tr>
-            <td> {{ ($loop->index) + 1 }} </td>
-            <td>
-                {{ $theme->name }}
-            </td>
-            <td> 
-                <a href="{{ route('forum.topics',$theme->id) }}" class="btn btn-outline-secondary">{{ $theme->topics->count() }}</a>
-            </td>
-        </tr>
-    @empty
-        <div>Aucunes références n'ont été trouvées >:C </div>
-    @endforelse
-    </tbody>
-</table>
-
+    @if (!$themes->isEmpty())
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nom</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($themes as $theme)
+                    <tr>
+                        <td> {{ ($loop->index) + 1 }} </td>
+                        <td>
+                            {{ $theme->name }}
+                        </td>
+                        <td> 
+                            <a href="{{ route('forum.topics',$theme->id) }}" class="btn btn-outline-secondary">{{ $theme->topics->count() }}</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @else
+        <div class="p-3 mb-2 bg-light text-dark"> Aucuns thèmes n'existent actuellement. :'c </div>
+    @endif
 @endsection
