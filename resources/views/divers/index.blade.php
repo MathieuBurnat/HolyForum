85fe1776f67ff726be8a001764ff308c70b81975
Mathieu <70205357+MathieuBurnat@users.noreply.github.com>
2021-03-05 12:16:31 +0100

    Upgrade the divers's view

--- resources/views/divers/index.blade.php
+++ resources/views/divers/index.blade.php
@@ -1,9 +1,17 @@
 @extends ('layout')
 
 @section ('content')
-    <div class="text-center mt-5 ">
-        <h1 class="fst-italic"> Hey!</h1>
-        <h2 class="text-warning"> Section : Divers</h2>
-        Salut toi, ça va ?
+    <div class="jumbotron m-1">
+        <h1 class="display-4">Divers </h1>
+        <hr class="my-4">
+        
+        <h3> Mathieu </h3>
+        <p class="m-2"> Jeune étudiant en informatique. Je suis toujours prenneur de bons conseils et de critiques constructives !</p>
+        
+        <h3> HolyForum </h3>
+        <p class="m-2"> Ce projet est a été implémenté avec Laravel et Bootstrap, vous pouvez obtenir son repos git <a href="https://github.com/MathieuBurnat/HolyForum">là</a>. </p>
+        
+        <h3> CPNV-ES </h3>
+        <p class="m-2"> HolyForum est basé sur un projet existant créé par Mr. Carrel, durant nos cours de PRW2. Vous pouvez obtenir le lien de son projet en cliquant <a href="https://github.com/XCarrel/myforum">ici</a>. </p>
     </div>
 @endsection
