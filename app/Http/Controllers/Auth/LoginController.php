2622405fb368d1cc013fc1e3c6c0406aa2e50f23
MathieuBurnat <70205357+MathieuBurnat@users.noreply.github.com>
2021-03-06 08:57:12 +0100

    Update dirties

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -35,7 +35,7 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        Auth::login($user);
+        //Auth::login($user);
         $this->middleware('guest')->except('logout');
     }
 }
